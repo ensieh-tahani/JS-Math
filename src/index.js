9effe21db8bf33787e87ef7ad8225e6e26e9e9e7
ensieh-tahani <ensieh.tahani@gmail.com>
2022-11-14 23:39:24 +0330

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+let price = 4.5;
+//challenge 1
+console.log(Math.round(price));
+
+//challenge 2
+console.log(Math.ceil(price));
+//challenge 3
+console.log(Math.floor(price));
+//challenge 4
+console.log(Math.min(4, 1, 3, 10));
+// Challenge 5
+console.log(Math.max(4, 1, 3, 10));
